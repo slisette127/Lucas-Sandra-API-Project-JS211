a04231538082da4b3fdec8f96bb8d3be966199df
slisette127 <slisette127@gmail.com>
2021-06-15 22:59:38 -0500

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+const pokemonListContainer = document.getElementById('pokemon-list');
+
+const URL = ` https://pokeapi.co/api/v2/pokemon/`;
+
+
+fetch(URL).then((response) => response.json()).then((data) => {
+  console.log(data.results)
+  data.results.forEach((pokemon) => {
+    console.log(pokemon);
+  const li = document.createElement('li');
+  const textNode = document.createTextNode(pokemon.name);
+      li.appendChild(textNode); 
+      pokemonListContainer.appendChild(li)  
+  })  
+  
+ 
+
+})
